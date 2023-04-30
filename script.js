98cfa6ecd377bb4b2cd600a09d3e50bacdd92cff
karthik <2128444>
2023-05-01 02:40:39 +0530

    phase1extended

---
+++ script.js
@@ -0,0 +1,45 @@
+var myVar;
+var wel;
+const typed = new Typed(".auto-input", {
+  strings: ["Hello, I'm Karthik"],
+  startDelay: 4000,
+  typeSpeed: 100,
+});
+
+function myFunction() {
+  //document.body.style.backgroundColor = "#03001C";
+  document.getElementById("welcome").style.display = "none";
+  document.getElementById("loader").style.display = "block";
+  myVar = setTimeout(showwelcm, 1500);
+
+  //document.body.style.backgroundColor = "#FAF8F1";
+}
+function showwelcm() {
+  // document.body.style.backgroundColor = "#03001C";
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("welcome").style.display = "block";
+  wel = setTimeout(showPage, 1500);
+}
+
+function showPage() {
+  //document.body.style.backgroundColor = "#000000";
+  document.getElementById("welcome").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
